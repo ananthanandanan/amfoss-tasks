69ab5e47b460f743ecb456c1c513bdacc2a5b7cf
ananthanandanan <ananthanandanan@gmail.com>
2019-08-19 21:51:59 +0530

    web upgrade

---
+++ task-2/Codeforces/codeforcetask4.cpp
@@ -0,0 +1,25 @@
+#include <iostream.h>
+#include <string.h>
+
+int main()
+{
+    int n, x=0;
+    cin >> n;
+
+    string s;
+    while (n--)
+    {
+        cin >> s;
+        if (s[1] == '+')
+        {
+            ++x;
+        }
+        else
+        {
+            --x;
+        }
+    }
+
+    cout << x << endl;
+    return 0;
+}
