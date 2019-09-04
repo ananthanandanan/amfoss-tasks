99346df2e16f42f0246e65d4617758974866a1c0
ananthanandanan <ananthanandanan@gmail.com>
2019-09-04 22:32:35 +0530

    ruby update

--- task-3/scrap/scraper.rb
+++ task-3/scrap/scraper.rb
@@ -1,38 +1,21 @@
-require "httparty"
+
 require "nokogiri"
-require "byebug"
 
-def scraper
-
- url = "https://en.wikipedia.org/wiki/Morgan_Freeman"
-
- unparsed_site = HTTParty.get(url)
-
- parsed_site = Nokogiri::HTML(unparsed_site)
-
- films = parsed_site.css('div.div-col')
- films.each do |films|
-
-   film = {
-
-
-names: films.css('li').text
-
-   }
-
-   byebug
-
- end
+require "open-uri"
 
 
 
 
 
+
+ doc = Nokogiri::HTML(open("https://www.google.com/search?ei=H65vXcmjJ8qNvQSx_ZqYBQ&q=linux&oq=linux&gs_l=psy-ab.3..35i39l2j0i67l8.8029469.8031744..8032140...1.3..0.1199.1199.7-1......0....1..gws-wiz.....10..0i71.jtEbAY6-cck&ved=0ahUKEwiJr6nVlbfkAhXKRo8KHbG-BlMQ4dUDCAs&uact=5"))
+
+doc.xpath( doc.xpath("(//div/a/div[@class'ellip'])[1]")).each do |linux|
+
+ puts linux.content
 
 
 
 
 
 end
-
-scraper
